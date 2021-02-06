d64446c8340bc9d8fd0504784a959fc25ca16a92
Abiramiravi95 <candybar03@yahoo.com>
2021-02-06 18:27:09 +0530

    selvam works

--- src/test/java/org/sample/Sample.java
+++ src/test/java/org/sample/Sample.java
@@ -5,4 +5,13 @@ public class Sample {
 	private void add() {
 		System.out.println("addd");
 	}
+	
+	public void divison() {
+		System.out.println("division");
+
+	}
+	private void trignometry() {
+		System.out.println("trignmetry");
+
+	}
 }
